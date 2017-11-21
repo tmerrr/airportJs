84f5ed730d8f8d58303aa0c1bd82757e70005e59
Tom Moir <trmoir9@gmail.com>
2017-11-21 14:32:51 +0000

    planes can land and take off from airports

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,6 +1,34 @@
 describe('airport', function() {
-  var airport = new Airport
+
+  beforeEach(function(){
+    airport = new Airport;
+    plane = new Plane;
+  });
+
   it('should have no planes', function() {
     expect(airport.planes).toEqual([]);
   });
+
+  it('should be able to land a plane: Plane is added to planes array', function() {
+    airport.receivePlane(plane);
+    expect(airport.planes).toEqual([plane]);
+  });
+
+  it('should be able to land a plane: tells plane to land', function() {
+    airport.receivePlane(plane);
+    expect(plane.isFlying()).toBe(false)
+  });
+
+  it('plane should be able to take off: Plane is removed from planes array', function() {
+    airport.receivePlane(plane);
+    airport.takeOffPlane(plane);
+    expect(airport.planes).toEqual([]);
+  });
+
+  it('plane should be able to take off: tells plane to take off', function() {
+    airport.receivePlane(plane);
+    airport.takeOffPlane(plane);
+    expect(plane.isFlying()).toBe(true)
+  });
+
 });
--- src/airport.js
+++ src/airport.js
@@ -1,3 +1,16 @@
 function Airport() {
   this.planes = [];
 }
+
+Airport.prototype = {
+  receivePlane: function(plane) {
+    this.planes.push(plane);
+    plane.land();
+  },
+
+  takeOffPlane: function(plane) {
+    var planeIndex = this.planes.indexOf(plane);
+    this.planes.splice(planeIndex, 1);
+    plane.takeOff();
+  }
+}
